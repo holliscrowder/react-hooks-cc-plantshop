65b9c3c2384405a2f7cc5e2ddcf51304a8aa9ea0
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-09 10:53:31 -0700

    core deliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ plants, search }) {
+  const plantList = plants
+    .filter((plant) => {
+      return plant.name.toLowerCase().includes(search.toLowerCase());
+    })
+    .map(({ id, name, image, price }) => {
+      return <PlantCard key={id} name={name} image={image} price={price} />;
+    });
+
+  return <ul className="cards">{plantList}</ul>;
 }
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchPlants = async () => {
+      const response = await fetch("http://localhost:6001/plants");
+      const plantData = await response.json();
+      setPlants(plantData);
+    };
+    fetchPlants();
+  }, []);
+
+  function onAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={plants} search={search} />
     </main>
   );
 }
